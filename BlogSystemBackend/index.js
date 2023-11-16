5053065ba287bf2a5906111b737957113f468819
seliyak <seliyak@yaratechnologies.com>
2023-11-17 00:03:17 +0530

    mongo db connection done

--- BlogSystemBackend/index.js
+++ BlogSystemBackend/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Could not connect to MongoDB", err));
+
+app.use(express.json());
+
 
 app.use(express.json()); // For parsing application/json
 
